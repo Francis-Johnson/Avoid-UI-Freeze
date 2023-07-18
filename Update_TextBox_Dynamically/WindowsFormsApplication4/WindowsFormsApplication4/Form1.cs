7944eb2b2bde23c02e9c315e53a322076ca9dbea
Francis Johnson <francisjohnson93@gmail.com>
2023-07-18 21:57:17 +0530

    first commit

---
+++ Update_TextBox_Dynamically/WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        private ProcessHandler ph = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ph = new ProcessHandler();
+        }
+
+        public void UpdateStatus(string timeMessage)
+        {
+            textBox1.Text = timeMessage;
+            Application.DoEvents();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            ph.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(Execute));
+            t.Start();
+        }
+
+        private void Execute()
+        {
+
+            ph.ProgressChanged += ph_ProgressChanged;
+            ph.RunTest();
+        }
+
+        void ph_ProgressChanged(object sender, EventArgs e)
+        {
+            UpdateProgressEventArgs arg = e as UpdateProgressEventArgs;
+
+            if (this.textBox1.InvokeRequired)
+            {
+                this.textBox1.BeginInvoke((MethodInvoker)delegate()
+                {
+                    this.textBox1.Text = arg.Message;
+                });
+            }
+            else
+            {
+                this.textBox1.Text = arg.Message;
+            }
+        }
+
+    }
+}
---
+++ Update_TextBox_Dynamically/WindowsFormsApplication4/WindowsFormsApplication4/ProcessHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication4
+{
+
+    public class UpdateProgressEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+    }
+
+    public class ProcessHandler
+    {
+        //private Form1 mainForm = null;
+        private int counter = 0;
+
+        public event EventHandler ProgressChanged;
+
+        private System.Timers.Timer timer = null;
+
+        public ProcessHandler()
+        {
+            //this.mainForm = from;
+        }
+
+        public void RunTest()
+        {
+            timer = new System.Timers.Timer(3000);
+            //timer.AutoReset = true;
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            counter++;
+            //mainForm.UpdateStatus(counter.ToString());
+
+            UpdateProgressEventArgs arg = new UpdateProgressEventArgs() { Message = counter.ToString() };
+
+            ProgressChanged(this, arg);
+        }
+
+        internal void Stop()
+        {
+            if (timer != null)
+            {
+                timer.Elapsed -= timer_Elapsed;
+                timer.Stop();
+            }
+        }
+    }
+}
